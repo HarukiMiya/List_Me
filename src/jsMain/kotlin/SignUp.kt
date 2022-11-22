8905cc1c85052b9e03f21349ca54a0e0f407a894
Victor <87928341+VMSandoval25@users.noreply.github.com>
2022-11-22 03:17:12 -0800

    Checks if User Exists

--- src/jsMain/kotlin/SignUp.kt
+++ src/jsMain/kotlin/SignUp.kt
@@ -1,4 +1,5 @@
 import csstype.AlignItems.Companion.center
+import csstype.ClassName
 import csstype.Display
 import csstype.FlexDirection.Companion.column
 import emotion.react.css
@@ -7,12 +8,17 @@ import kotlinx.coroutines.launch
 import react.*
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.p
+import react.router.useNavigate
 
 private val scope = MainScope()
 
 
 val SignUp = FC<Props> {
+    val navigate = useNavigate()
     var user by useState(emptyList<User>())
+
+    val (exist, setExist) = useState(false)
 
     useEffectOnce {
         scope.launch {
@@ -32,17 +38,30 @@ val SignUp = FC<Props> {
             }
         }
         signUpComponent {
-
             onSubmit= { input, input2 -> // differet onSubmit than the other??
                 val userinfo = User(input,input2)
                 console.log(input)
                 console.log(input2)
                 scope.launch {
-                    addUser(userinfo)
-                    user = getUser()
+                    if(searchUser(userinfo) == "False"){
+                        addUser(userinfo)
+                        navigate("/login")
+                    }
+                    else{
+                        setExist(true)
+                    }
                 }
             }
         }
+        if(exist){
+            p{
+                id = "exists"
+                +"Username already taken"
+            }
+
+        }
+
+
     }
 }
 
